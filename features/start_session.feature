81a99dfe7a04f806e476aeda6c1f95308e09b8a9
troystribling-att <troy.stribling@usi.net>
2010-02-09 17:39:52 -0500

    matchy works, wrote object attribute pattern manager and finished user and admin session test cases.

--- features/start_session.feature
+++ features/start_session.feature
@@ -9,3 +9,15 @@ Feature: Start CaaS Session
     | attribute      | value |
     | authentication |   *   |
     
+
+  Scenario: A user provides valid authentication credentials
+    Given a valid "user" user ID and a valid password
+    Then a valid CaaS "user" session can be created with the following attributes
+    | attribute       | value               |
+    | authentication  |   *                 |
+    | account_uri     | /locations          |
+    | clouds_uri      | /accounts/*/clouds  |
+    | locations_uri   | /locations          | 
+    | vmtemplates_uri | /vmtemplates        |
+    | onboard_uri     | /accounts/*/onboard |
+
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,10 @@ require 'shoulda'
 require 'matchy'
 
 ####------------------------------------------------------------------------------------------------------
+require 'test/unit/assertions'
+World(Test::Unit::Assertions)
+
+####------------------------------------------------------------------------------------------------------
 module CaasHelpers
 
   def load_credentials(role)
@@ -17,5 +21,11 @@ World(CaasHelpers)
 
 ####------------------------------------------------------------------------------------------------------
 def_matcher :match_pattern do |receiver, matcher, args|
-  true
+  matcher.positive_msg = "Expected patern match between #{receiver} and '#{args.first}'"
+  matcher.negative_msg = "Expected no patern match between #{receiver} and '#{args.first}'"
+  unless args.first.eql?('*')
+    pat = args.first.gsub('*','')
+    url = receiver.gsub(/\d*/,'')
+    pat.eql?(url)
+  else;true;end
 end
